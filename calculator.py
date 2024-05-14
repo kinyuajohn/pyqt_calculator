c62ed1d37e2366daaaa6135fa55a9afc7bc3eb4c
John Kinya <kingjohnkn@gmail.com>
2024-05-14 03:40:55 +0300

    finalize app

--- calculator.py
+++ calculator.py
@@ -31,10 +31,37 @@ buttons = [
 clear = QPushButton("clear")
 delete = QPushButton("del")
 
+
+def button_click():
+    button = app.sender()
+    text = button.text()
+
+    if text == "=":
+        symbol = text_box.text()
+        try:
+            result = eval(symbol)
+            text_box.setText(str(result))
+
+        except Exception as e:
+            print("Error:", e)
+
+    elif text == "clear":
+        text_box.clear()
+
+    elif text == "del":
+        current_value = text_box.text()
+        text_box.setText(current_value[:-1])
+
+    else:
+        current_value = text_box.text()
+        text_box.setText(current_value + text)
+
+
+# Loop for creating buttons
 row, col = 0, 0
 for text in buttons:
     button = QPushButton(text)
-    # button.clicked.connect(None)
+    button.clicked.connect(button_click)
     grid.addWidget(button, row, col)
     col += 1
 
@@ -52,8 +79,10 @@ button_row.addWidget(clear)
 button_row.addWidget(delete)
 
 master_layout.addLayout(button_row)
-
 main_window.setLayout(master_layout)
+
+clear.clicked.connect(button_click)
+delete.clicked.connect(button_click)
 
 # Show/Run
 main_window.show()
